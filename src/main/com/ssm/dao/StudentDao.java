5614fad685040fdec98ad6be47abc1f2be7eff0d
taray <1143024837@qq.com>
2020-08-13 22:48:55 +0800

    自定义mybatis简单demo

--- src/main/com/ssm/dao/StudentDao.java
+++ src/main/com/ssm/dao/StudentDao.java
@@ -1,10 +1,15 @@
 package com.ssm.dao;
 
+import com.ssm.annotations.Select;
 import com.ssm.domain.Student;
 
 import java.util.List;
 
 public interface StudentDao {
+    //注解开发配置2
+    /*@Select("select * from Student")
+    List<Student> findAll();*/
+
     List<Student> findAll();
     void insertStudent(Student student);
     Student getStudentById(int id);
